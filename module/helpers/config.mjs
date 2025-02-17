2be217604fc18bfb84ce1667cd270b648dbb8d2f
phewi <phewnix@gmail.com>
2025-02-17 10:32:37 +0200

    first commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const RECON_QUEST = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+RECON_QUEST.abilities = {
+  str: 'RECON_QUEST.Ability.Str.long',
+  dex: 'RECON_QUEST.Ability.Dex.long',
+  con: 'RECON_QUEST.Ability.Con.long',
+  int: 'RECON_QUEST.Ability.Int.long',
+  wis: 'RECON_QUEST.Ability.Wis.long',
+  cha: 'RECON_QUEST.Ability.Cha.long',
+};
+
+RECON_QUEST.abilityAbbreviations = {
+  str: 'RECON_QUEST.Ability.Str.abbr',
+  dex: 'RECON_QUEST.Ability.Dex.abbr',
+  con: 'RECON_QUEST.Ability.Con.abbr',
+  int: 'RECON_QUEST.Ability.Int.abbr',
+  wis: 'RECON_QUEST.Ability.Wis.abbr',
+  cha: 'RECON_QUEST.Ability.Cha.abbr',
+};
